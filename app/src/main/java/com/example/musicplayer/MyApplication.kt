7590e18d7aa9aa6441dab9de01bfd5367773b869
chenhaoyang <1808413743@qq.com>
2022-12-19 17:40:02 +0800

    add func getMusicBinaryDataFromLC

---
+++ app/src/main/java/com/example/musicplayer/MyApplication.kt
@@ -0,0 +1,16 @@
+package com.example.musicplayer
+
+import android.app.Application
+import cn.leancloud.LeanCloud
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        LeanCloud.initialize(
+            this,
+            "PksCkBWumFbGrS4dtAFRZcGO-gzGzoHsz",
+            "C45TW07EO15WQIV1mcV6Ilol",
+            "https://pksckbwu.lc-cn-n1-shared.com"
+        )
+    }
+}
--- app/src/main/java/com/example/musicplayer/fragment/tabfragment/ListenFragment.kt
+++ app/src/main/java/com/example/musicplayer/fragment/tabfragment/ListenFragment.kt
@@ -1,18 +1,28 @@
 package com.example.musicplayer.fragment.tabfragment
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.musicplayer.R
 import com.example.musicplayer.databinding.FragmentListenBinding
 import com.example.musicplayer.utils.setChipWithEvent
 import com.example.musicplayer.utils.setFirstRecyclerView
+import com.example.musicplayer.viewmodel.ListenViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
 
 
 class ListenFragment : Fragment() {
     private lateinit var binding: FragmentListenBinding
+    private val model: ListenViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,6 +32,14 @@ class ListenFragment : Fragment() {
 
         setFirstRecyclerView(requireActivity(), binding)
 
+        lifecycleScope.launch(Dispatchers.IO) {
+            model.getMusicBinaryDataFromLC(requireContext())
+            withContext(Dispatchers.Main) {
+                val file = File(requireContext().filesDir.path+"mis")
+                val player = MediaPlayer.create(requireContext(), Uri.fromFile(file))
+                player.start()
+            }
+        }
         return binding.root
     }
 
