a1bef2c341f5a655834f0be8c1639c6a0a7e52d0
chenhaoyang <1808413743@qq.com>
2022-12-25 15:12:46 +0800

    add utils SongUtils and delete some unuseful methods

--- app/src/main/java/com/example/musicplayer/viewmodel/ListenViewModel.kt
+++ app/src/main/java/com/example/musicplayer/viewmodel/ListenViewModel.kt
@@ -1,18 +1,9 @@
 package com.example.musicplayer.viewmodel
 
 
-import android.media.AudioAttributes
-import android.media.MediaPlayer
-import android.util.Log
+
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import cn.leancloud.LCFile
-import cn.leancloud.LCQuery
-import io.reactivex.Observer
-import io.reactivex.disposables.Disposable
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+
 
 
 class ListenViewModel : ViewModel() {
